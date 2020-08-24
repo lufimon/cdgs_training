b9b755d1cf80c13aaa6785f37bb7d6141d48f9e7
Tanuphong Phoemtaweesantisuh <tanuphong.p@hotmail.com>
2020-08-24 16:50:56 +0700

    initial commit

---
+++ app/src/main/java/th/co/cdgs/mobile/cdgstraining/EditActivity.kt
@@ -0,0 +1,12 @@
+package th.co.cdgs.mobile.cdgstraining
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+    }
+}
